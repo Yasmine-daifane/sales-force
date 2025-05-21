11f75e4c7bf0fb22197fdaebe7ead8188369f41b
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2025-05-21 16:41:21 +0100

    push

--- tracking-sales/app/Http/Kernel.php
+++ tracking-sales/app/Http/Kernel.php
@@ -42,6 +42,8 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Fruitcake\Cors\HandleCors::class, // Assurez-vous que cette ligne est présente
+
         ],
     ];
 
